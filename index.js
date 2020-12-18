b7655dee00ad0b308410e48f4a908bb8958b5df4
RafikiSalmeron <rafikisalmeronmartos@gmail.com>
2020-12-18 12:35:15 +0100

    FINAL con AJAX

--- index.js
+++ index.js
@@ -66,7 +66,21 @@ console.log(ejercicio3_2([1, 2, 3, 4, 5]));
 
 
 function ejercicio4(filas, columnas, color, nodo) {
+  var texto;
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      texto = this.responseText;
+      crearTabla(filas, columnas, color, nodo, texto);
+    }
+  };
+  xhttp.open("GET", "textoEjercicio4.txt", true);
+  xhttp.send();
   //var division = document.getElementById(nodo);
+
+}
+
+function crearTabla(filas, columnas, color, nodo, texto){
   var tabla = document.createElement("table");
   tabla.setAttribute("border", "solid");
   for (let i = 0; i < filas; i++) {
@@ -76,7 +90,7 @@ function ejercicio4(filas, columnas, color, nodo) {
     }
     for (let p = 0; p < columnas; p++) {
       var td = document.createElement("td");
-      td.appendChild(document.createTextNode('Examen'));
+      td.appendChild(document.createTextNode(texto));
       td.addEventListener("click", function() {
         borrarTexto(event);
         localStorage.setItem("table", JSON.stringify(tabla.outerHTML));
@@ -93,5 +107,4 @@ function borrarTexto(event) {
   var target = event.target;
   target.innerHTML = "";
 }
-
 ejercicio4(5, 6, "red", document.getElementById("bloque1"));
